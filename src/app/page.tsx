c0b0a011cd5e69bbc070472236b888cbbd4def7e
jordeguevara <jordeguevara@gmail.com>
2025-06-09 22:27:35 -0700

    udpated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/workouts"); // Redirect to the /workouts page
+  }, [router]);
   return <div className="bg-white text-black"></div>;
 }
